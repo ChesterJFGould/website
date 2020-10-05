dd64a5d911e73b5661c9e3b27bce15e829aa928e
Techser <chestergould@protonmail.com>
2020-10-04 22:48:57 -0700

    updated main.go

--- main.go
+++ main.go
@@ -7,10 +7,18 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "html/index.html")	
+	http.ServeFile(w, r, "html/index.html")
+	log.Println("index.html")
 }
 
 func main() {
+	logFile, err := os.OpenFile("website.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetOutput(logFile)
+
 	http.HandleFunc("/", index)
 
 	http.ListenAndServe(":80", nil)

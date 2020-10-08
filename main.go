7b40c9e9968976763a27ea936ca39e6153636cad
Techser <chestergould@protonmail.com>
2020-10-08 09:34:58 -0700

    added user agent to server

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func favicon(w http.ResponseWriter, r *http.Request) {
 
 func blog(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "html/blog.html")
-	log.Printf("blog.html %s\n", r.RemoteAddr)
+	log.Printf("blog.html %s %s\n", r.RemoteAddr, r.UserAgent())
 }
 
 func math(w http.ResponseWriter, r *http.Request) {
